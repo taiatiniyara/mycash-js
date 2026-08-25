bd2863b58a19fe41761200c7b86c4d0d55f6d768
taiatiniyara <taiatiniyara@gmail.com>
2026-08-26 10:18:29 +1200

    docs: add self-hosted Inter and JetBrains Mono variable fonts
    - Install @fontsource-variable/inter and @fontsource-variable/jetbrains-mono
    - Wire fonts through theme imports so Vite bundles only needed woff2 files
    - Set base/mono font-family CSS variables
    - Typography polish: stylistic sets, heading letter-spacing, code sizing

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,6 @@
 import DefaultTheme from "vitepress/theme";
+import "@fontsource-variable/inter";
+import "@fontsource-variable/jetbrains-mono";
 import "./custom.css";
 
 export default DefaultTheme;
